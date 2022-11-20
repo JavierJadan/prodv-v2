b0ce4f57afab36edf70eea9aec73a03f4c5160b1
jhongo <jhon_gom22@outlook.com>
2022-11-20 14:58:15 -0500

    New Style in profile

--- src/app/pages/page-internacional/page-internacional.page.ts
+++ src/app/pages/page-internacional/page-internacional.page.ts
@@ -26,16 +26,16 @@ export class PageInternacionalPage implements OnInit {
 
   ngOnInit() {
 
-    this.http.get<any>('https://v3.football.api-sports.io/fixtures?live=all',{
-      headers:{
-        'x-rapidapi-host' : 'v3.football.api-sports.io',
-        'x-rapidapi-key'  : '057eb7678fa2d34dca7c319ce9a57a7d'
-      }
-    }).subscribe(res =>{
-      console.log(res);
-      this.matches = res.response;
-      console.log(res.response);
-    });
+    // this.http.get<any>('https://v3.football.api-sports.io/fixtures?live=all',{
+    //   headers:{
+    //     'x-rapidapi-host' : 'v3.football.api-sports.io',
+    //     'x-rapidapi-key'  : '057eb7678fa2d34dca7c319ce9a57a7d'
+    //   }
+    // }).subscribe(res =>{
+    //   console.log(res);
+    //   this.matches = res.response;
+    //   console.log(res.response);
+    // });
   }
   // async initialize(){
   //   const { status } = await AdMob.trackingAuthorizationStatus();
