10014b9cb5142ceabca9ca4f30036e592b4c114d
jhongo <jhon_gom22@outlook.com>
2024-02-23 22:49:44 -0500

    👔 [ADD]: GetData matches
    [ADD]: GetData matches

--- src/app/detail-intl/detail-intl.page.ts
+++ src/app/detail-intl/detail-intl.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-detail-intl',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailIntlPage implements OnInit {
 
-  constructor() { }
+  detailId : string;
+  matchesPL = [];
+
+  constructor(
+    private activateddRouter: ActivatedRoute, 
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.detailId = this.activateddRouter.snapshot.paramMap.get('id');
+    console.log(this.detailId);
+
+    this.http.get<any>('https://apiv3.apifootball.com' ,{
+      params:{
+        APIkey: '2335c07ce6ca12a93dde8b639488bbeba8a2281f6eb351e53153d261d3d23e43',
+        action: 'get_events',
+        league_id: this.detailId,
+        from:'2023-08-01',
+        to:'2024-05-19' 
+      }
+    }).subscribe(res =>{
+
+      if (res) {
+        // console.log(res);
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        this.matchesPL = res;
+
+        console.log(this.matchesPL);
+
+      }else{
+        console.log('Dont get DATA');
+      }
+    });
   }
 
 }
